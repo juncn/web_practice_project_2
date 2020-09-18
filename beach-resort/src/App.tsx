0192e0faa38e78f795523968c63ddf60c1dd8b11
JunmingGuo <sjunmg@gmail.com>
2020-09-17 23:09:06 -0400

    add navbar and routing

---
+++ beach-resort/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Style
+import './App.css';
+
+// Pages
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+// Component
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:id"  exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ beach-resort/src/pages/SingleRoom.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  render() {
+    return <div>Single Room</div>
+  }
+}
+
+export default SingleRoom;
