58f90947f0e24b5eaa27b3d8a84fa8257dd4d02d
JunmingGuo <sjunmg@gmail.com>
2020-09-22 00:46:05 -0400

    add StyledHero to dynamically set hero image use StyledHero component to dynamically set hero image for a single room

---
+++ beach-resort/src/components/StyledHero.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import defaultBcg from '../assets/images/room-1.jpeg';
+
+type Props = {
+  img: string;
+};
+
+const StyledHero = styled.header<Props>`
+  min-height: 66vh;
+  background: url(${props => props.img || defaultBcg}) center/cover no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default StyledHero;
--- beach-resort/src/pages/SingleRoom.tsx
+++ beach-resort/src/pages/SingleRoom.tsx
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import Banner from '../components/Banner';
+import StyledHero from '../components/StyledHero';
+import { RoomContext } from '../Context';
 
-class SingleRoom extends Component {
+interface MatchParams {
+  slug: string;
+}
+
+interface Props extends RouteComponentProps<MatchParams> {}
+
+type State = {
+  slug: string;
+};
+
+class SingleRoom extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+    };
+  }
+
+  static contextType = RoomContext;
+
   render() {
-    return <div>Single Room</div>
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return !room ? (
+      <div className="error">
+        <h3>No such room could be found</h3>
+        <Link to="/rooms" className="btn-primary">
+          Back to rooms
+        </Link>
+      </div>
+    ) : (
+      <StyledHero img={images[0]}>
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </StyledHero>
+    );
   }
 }
 
