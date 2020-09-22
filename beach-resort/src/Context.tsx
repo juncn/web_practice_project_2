fb3114b44b399ff2974840c97a821af0028f6505
JunmingGuo <sjunmg@gmail.com>
2020-09-21 23:41:48 -0400

    Update Context 1. use Partial to allow empty default value in createContext 2. add return type for functions

--- beach-resort/src/Context.tsx
+++ beach-resort/src/Context.tsx
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import items from './assets/data';
 
 type Room = {
-  breakfast: boolean
-  capacity: number
-  description: string
-  extras: []
-  featured: boolean
-  id: string
-  images: []
-  name: string
-  pets: boolean
-  price: number
-  size: number
-  slug: string
-  type: string
-}
+  breakfast: boolean;
+  capacity: number;
+  description: string;
+  extras: [];
+  featured: boolean;
+  id: string;
+  images: [];
+  name: string;
+  pets: boolean;
+  price: number;
+  size: number;
+  slug: string;
+  type: string;
+};
 
-const RoomContext = React.createContext({
-  rooms: [],
-  sortedRooms: [],
-  featuredRooms: [],
-  loading: true,
-});
+type ContextProps = {
+  rooms: Room[];
+  sortedRooms: Room[];
+  featuredRooms: Room[];
+  loading: boolean;
+  getRoom: (slug: string) => Room | undefined;
+};
+
+const RoomContext = React.createContext<Partial<ContextProps>>({});
 
 class RoomProvider extends Component {
   state = {
@@ -34,7 +37,6 @@ class RoomProvider extends Component {
 
   componentDidMount() {
     let rooms = this.formatData(items);
-    console.log(rooms);
     let featuredRooms = rooms.filter((room: Room) => room.featured === true);
     this.setState({
       rooms,
@@ -44,7 +46,7 @@ class RoomProvider extends Component {
     });
   }
 
-  formatData = (items: any) => {
+  formatData = (items: any): Room[] => {
     let tempItems = items.map((item: any) => {
       let id = item.sys.id;
       let images = item.fields.images.map((img: any) => img.fields.file.url);
@@ -54,9 +56,15 @@ class RoomProvider extends Component {
     return tempItems;
   };
 
+  getRoom = (slug: string): Room | undefined => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room: Room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
