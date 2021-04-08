12f054eb1df8576d88983aae913f7bb0a7dbdd55
JunmingGuo <sjunmg@gmail.com>
2021-04-07 21:20:12 -0700

    Implement RoomList component

--- beach-resort/src/components/RoomList.tsx
+++ beach-resort/src/components/RoomList.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Room } from '../types';
+import RoomCard from './RoomCard';
 
 interface Props {
-  rooms?: Room[];
+  rooms: Room[];
 }
 
-const RoomsList = ({ rooms }: Props) => <div>Room List</div>
+const RoomsList = ({ rooms }: Props) => {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>Unfortunately no rooms matched your search parameters</h3>
+      </div>
+    );
+  }
+
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map(room => {
+          return <RoomCard key={room.id} room={room} />
+        })}
+      </div>
+    </section>
+  );
+}
 
 export default RoomsList;
